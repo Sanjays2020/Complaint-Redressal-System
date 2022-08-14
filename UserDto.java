3ea6e1fe8cad8b845f25a00c073f73a7ed3f7828
Sanjays2020 <96116812+Sanjays2020@users.noreply.github.com>
2022-08-14 16:44:56 +0530

    Create UserDto.java

---
+++ UserDto.java
@@ -0,0 +1,15 @@
+package com.vikram.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+	
+	private String email;
+	private String password;
+
+}
+Footer
+© 2022 GitHub,
